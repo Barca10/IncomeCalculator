53f7089c7253b077a2cbbbb28707e67ad3ebeb34
Barca10 <41929876+Barca10@users.noreply.github.com>
2018-10-10 22:18:14 -0500

    re-commit (#2)

---
+++ IncomeCalculator/IncomeCalculateService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IncomeCalculator
+{
+   public class IncomeCalculateService
+    {
+        public int HoursWorkedPerDay = 8;
+
+        public int NumberOfDaysWorked = 261;
+
+        public decimal IncomeAfterCommisionPaid(decimal ratePerHour)
+        {
+            decimal _annualIncome = 0;
+            //Get the income per year without taxes
+            _annualIncome = ratePerHour * HoursWorkedPerDay * NumberOfDaysWorked;
+
+            _annualIncome = ((_annualIncome * Rates.ConsultantRate)/100);
+
+            return _annualIncome;
+
+        }
+    }
+}
---
+++ IncomeCalculator/RateValidator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace IncomeCalculator
+{
+   public class RateValidator
+    {
+        public bool ValidateRate(string input)
+        {
+            
+            // Regex _regex = new Regex(@"/^\d+\.?\d*$/");
+            decimal value;
+            if(Decimal.TryParse(input, out value) && !string.IsNullOrEmpty(input)  && !string.IsNullOrWhiteSpace(input))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
